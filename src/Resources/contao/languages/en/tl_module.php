df8dd529258a9007c07b54ba7f843fcac228c630
Georg Scherg <georg.scherg@gmx.de>
2018-10-26 14:14:17 +0200

    Bugfixes

--- src/Resources/contao/languages/en/tl_module.php
+++ src/Resources/contao/languages/en/tl_module.php
@@ -12,17 +12,9 @@ $GLOBALS['TL_LANG']['tl_module']['gb_perPage']        = array('Einträge pro Sei
 $GLOBALS['TL_LANG']['tl_module']['gb_moderate']       = array('Moderieren', 'Einträge bestätigen, bevor sie auf der Webseite veröffentlicht werden.');
 $GLOBALS['TL_LANG']['tl_module']['gb_bbcode']         = array('BBCode erlauben', 'Besuchern das Formatieren ihrer Einträge mittels BBCode erlauben.');
 $GLOBALS['TL_LANG']['tl_module']['gb_disableCaptcha'] = array('Sicherheitsfrage deaktivieren', 'Hier können Sie die Sicherheitsfrage abschalten (nicht empfohlen).');
-<<<<<<< HEAD
-<<<<<<< HEAD
 $GLOBALS['TL_LANG']['tl_module']['gb_disableURL']     = array('Formularfeld: Homepage deaktivieren', 'Hier können Sie dieses Eingabefeld abschalten.');
 $GLOBALS['TL_LANG']['tl_module']['gb_disableDSGVO']   = array('DSGVO Hinweis deaktivieren', 'Hier können Sie dieses Informationsfeld abschalten.');
 $GLOBALS['TL_LANG']['tl_module']['gb_DSGVOpagelink']  = array('Link zur Datenschutzerklärung', 'Hier können Sie den Link zur Datenschutzseite eingeben.');
-=======
-=======
->>>>>>> 45ea18e084ff93b27a4ab203025d73bd450a9467
-
-$GLOBALS['TL_LANG']['tl_module']['gb_disableURL'] = array('Formularfeld: Homepage deaktivieren', 'Hier können Sie dieses Eingabefeld abschalten.');
->>>>>>> 45ea18e084ff93b27a4ab203025d73bd450a9467
 $GLOBALS['TL_LANG']['tl_module']['gb_template']       = array('Template', 'Hier können Sie das Template der Einträge auswählen.');
 $GLOBALS['TL_LANG']['tl_module']['gb_jumpTo']         = array('Redirect Einstellung', 'Hier können Sie die Seite auswählen zu der der User gelangt nachdem das Formular abgeschickt wurde.');
 
