3532078991b99d993fc889139e271884ab730629
Seefahrer <georg.scherg@gmx.de>
2022-08-27 19:33:31 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -6,30 +6,25 @@
  * @license LGPL-3.0-or-later
  */
 
-
-if (TL_MODE == 'FE')
-{
+if (System::getContainer()->get('contao.routing.scope_matcher')->isFrontendRequest(System::getContainer()->get('request_stack')->getCurrentRequest())) {
     $GLOBALS['TL_CSS'][]  = 'bundles/seefahrerguestbook/guestbook.min.css';
 }
 
 // Front end modules
-array_insert($GLOBALS['FE_MOD'], 1, array
-(
-    'guestbook' => array
-    (
-        'guestbooklist' => 'GuestBook',
-        'guestbookform' => 'GuestBookForm'
+array_insert($GLOBALS['FE_MOD'], 1, array (
+        'guestbook' => array (
+            'guestbooklist' => 'GuestBook',
+            'guestbookform' => 'GuestBookForm'
+        )
     )
-));
+);
 
 // Back end modules
-array_insert($GLOBALS['BE_MOD']['content'], 2, array
-(
-    'guestbook' => array
-    (
-        'tables' => array('tl_guestbook'),
-        'icon'   => 'bundles/seefahrerguestbook/icon.png',
-        'stylesheet' => 'bundles/seefahrerguestbook/style.css'
+array_insert($GLOBALS['BE_MOD']['content'], 2, array (
+        'guestbook' => array (
+            'tables' => array('tl_guestbook'),
+            'icon'   => 'bundles/seefahrerguestbook/icon.png',
+            'stylesheet' => 'bundles/seefahrerguestbook/style.css'
+        )
     )
-));
-?>
+);
