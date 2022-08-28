f8a247570e547860ee6eb98c52deb0e56a58203d
Seefahrer <georg.scherg@gmx.de>
2022-08-28 13:59:45 +0200

    Update GuestBookForm.php

--- src/Resources/contao/classes/GuestBookForm.php
+++ src/Resources/contao/classes/GuestBookForm.php
@@ -285,7 +285,7 @@ class GuestBookForm extends Module {
         $objEmail->sendTo($GLOBALS['TL_ADMIN_EMAIL']);
 
         // Redirect
-        if (mb_strlen($this->gb_jumpTo)) {
+        if (strlen($this->gb_jumpTo)) {
             $objNextPage = $this->Database->prepare("SELECT id, alias FROM tl_page WHERE id=?")->limit(1)->execute($this->gb_jumpTo);
             if ($objNextPage->numRows) {
                 $this->redirect($this->generateFrontendUrl($objNextPage->fetchAssoc()));
