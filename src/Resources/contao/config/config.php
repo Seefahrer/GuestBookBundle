a6d7769352acd2c12618c719a3f379ab90e950ac
Seefahrer <georg.scherg@gmx.de>
2022-08-28 11:30:26 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -5,25 +5,8 @@
 *
 * @license LGPL-3.0-or-later
 */
-
  
-use Contao\CoreBundle\Routing\ScopeMatcher;
-use Symfony\Component\HttpFoundation\RequestStack;
-
-private ScopeMatcher $scopeMatcher;
-private RequestStack $requestStack;
-
-public function __construct(ScopeMatcher $scopeMatcher, RequestStack $requestStack)
-{
-    $this->scopeMatcher = $scopeMatcher;
-    $this->requestStack = $requestStack;
-}
-
-$request = $this->requestStack->getCurrentRequest();
-
-if ($this->scopeMatcher->isFrontendRequest($request)) {
-    $GLOBALS['TL_CSS'][]  = 'bundles/seefahrerguestbook/guestbook.min.css';
-}
+$GLOBALS['TL_CSS'][]  = 'bundles/seefahrerguestbook/guestbook.min.css';
 
 // Front end modules
 array_insert($GLOBALS['FE_MOD'], 1, array (
