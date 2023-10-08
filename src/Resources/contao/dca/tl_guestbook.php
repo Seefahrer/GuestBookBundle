7874becd3c39f69fe8fbd0cf010687e3222f258e
Seefahrer <georg.scherg@gmx.de>
2023-10-08 17:44:45 +0200

    Update tl_guestbook.php

--- src/Resources/contao/dca/tl_guestbook.php
+++ src/Resources/contao/dca/tl_guestbook.php
@@ -39,7 +39,7 @@ $GLOBALS['TL_DCA']['tl_guestbook'] =  [
                 'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
                 'href'                => 'act=select',
                 'class'               => 'header_edit_all',
-                'attributes'          => 'onclick="System::Backend.getScrollOffset();"'
+                'attributes'          => 'onclick="Backend.getScrollOffset();"'
             )
         ),
         'operations' => array (
@@ -52,12 +52,12 @@ $GLOBALS['TL_DCA']['tl_guestbook'] =  [
                 'label'               => &$GLOBALS['TL_LANG']['tl_guestbook']['delete'],
                 'href'                => 'act=delete',
                 'icon'                => 'delete.gif',
-                'attributes'          => 'onclick="if (!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\')) return false; System::Backend.getScrollOffset();"'
+                'attributes'          => 'onclick="if (!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\')) return false; Backend.getScrollOffset();"'
             ),
             'toggle' => array (
                 'label'               => &$GLOBALS['TL_LANG']['tl_guestbook']['toggle'],
                 'icon'                => 'invisible.gif',
-                'attributes'          => 'onclick="System::Backend.getScrollOffset(); return; AjaxRequest.toggleVisibility(this, %s);"',
+                'attributes'          => 'onclick="Backend.getScrollOffset(); return; AjaxRequest.toggleVisibility(this, %s);"',
                 'button_callback'     => array('tl_guestbook', 'toggleIcon')
             ),
             'show' => array (
