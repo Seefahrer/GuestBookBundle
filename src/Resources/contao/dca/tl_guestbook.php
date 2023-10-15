925cb09afff04479b4a8a47c4ca297120b1cd02f
Seefahrer <georg.scherg@gmx.de>
2023-10-15 12:57:26 +0200

    Update tl_guestbook.php

--- src/Resources/contao/dca/tl_guestbook.php
+++ src/Resources/contao/dca/tl_guestbook.php
@@ -197,7 +197,7 @@ class tl_guestbook extends Backend {
     
     // Toggle published icon
     public function toggleIcon($row, $href, $label, $title, $icon, $attributes) {
-        if (mb_strlen($this->Input->get('id'))) {
+        if (strlen($this->Input->get('id'))) {
             $this->toggleVisibility($this->Input->get('id'), ($this->Input->get('state') == 1));
             $this->redirect($this->getReferer());
         }
