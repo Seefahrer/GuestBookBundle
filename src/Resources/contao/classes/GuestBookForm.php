3c099631f867c0477e8e95a60c425ede6ff91450
Seefahrer <georg.scherg@gmx.de>
2023-10-19 09:48:46 +0200

    Update GuestBookForm.php

--- src/Resources/contao/classes/GuestBookForm.php
+++ src/Resources/contao/classes/GuestBookForm.php
@@ -130,7 +130,7 @@ class GuestBookForm extends Module {
             $objWidget = new $strClass(Widget::getAttributesFromDca($arrField, $arrField['name'], $arrField['value']));
             // Validate widget
             $session = Contao\System::getContainer()->get('request_stack')->getCurrentRequest()->getSession();
-            dd($session->get('contao.form.data')->getValue()['FORM_SUBMIT']);
+            dd($session->get('contao.form.name'));
             if (Input::post('FORM_SUBMIT') == 'tl_guestbook') {
                 $objWidget->validate();
                 if ($objWidget->hasErrors()) {
