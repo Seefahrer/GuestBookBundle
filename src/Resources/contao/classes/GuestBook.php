154e67cc4c1cf0d2eecdc8def3161e952da1562c
Seefahrer <georg.scherg@gmx.de>
2023-10-21 13:55:58 +0200

    Update GuestBook.php

--- src/Resources/contao/classes/GuestBook.php
+++ src/Resources/contao/classes/GuestBook.php
@@ -36,8 +36,7 @@ class GuestBook extends Module {
         $hasBackendUser = System::getContainer()->get('contao.security.token_checker')->hasBackendUser();
         // Pagination
         if ($this->gb_perPage > 0) {
-            $page = $this->Input->get('page') ? $this->Input->get('page') : 1;
-            dd($page);
+            $page = Input::get('page') ? Input::get('page') : 1;
             $limit = $this->gb_perPage;
             $offset = ($page - 1) * $this->gb_perPage;
             // Get total number of comments
